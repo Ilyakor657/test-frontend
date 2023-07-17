cecc5b4ace0a777316c81833953668a6e495ba0d
Ilya Korolev <82649037+Ilyakor657@users.noreply.github.com>
2023-07-17 17:30:05 +0400

    главная страница

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,77 @@
 import React, { useState } from "react";
-import HomeSwitch from '../components/HomeSwitch'
+import { Form, Button } from 'antd';
+import HomeSwitch from '../components/home/HomeSwitch'
+import FormIndividuals from '../components/forms/FormIndividuals'
+import FormLegal from '../components/forms/FormLegal'
+import FormCredit from '../components/forms/FormCredit'
+//import FormContribution from '../components/forms/FormContribution'
 
 function Home() {
   const [client, setClient] = useState(false)
-  const [product, setProduct] = useState(false)
+  const [product, setProduct] = useState(true)
+  const [loadingBtn, setLoadingBtn] = useState(false)
+
+  const send = async () => {
+    try {
+      setLoadingBtn(true)
+      setTimeout(() => {
+        setLoadingBtn(false)
+      }, 1000);
+    } catch (e) {
+      console.log(e);
+      setLoadingBtn(false)
+    }
+  }
 
   return (
     <section className="home">
-      <div className="home__client">
-        <HomeSwitch checked={'Физическое лицо'} unChecked={'Юридическое лицо'}/>
-        {client ? 
-          '' 
-        : 
-          ''
-        }
-      </div>
-      <div className="home__product">
-        <HomeSwitch checked={'Вклад'} unChecked={'Кредит'}/>
-        {product ? 
-          '' 
-        : 
-          ''
-        }
-      </div>
+      <Form
+        name="form"
+        layout="vertical"
+        onFinish={send}
+      >
+        <div className='forms'>
+          <div className="client">
+            <HomeSwitch 
+              checked={'Физическое лицо'} 
+              unChecked={'Юридическое лицо'}
+              switch={'client'}
+              default={client}
+              setClient={setClient}
+            />
+            {client ? 
+              <FormLegal />
+            : 
+              <FormIndividuals /> 
+            }
+          </div>
+          <div className="product">
+            <HomeSwitch 
+              checked={'Вклад'} 
+              unChecked={'Кредит'}
+              switch={'product'}
+              default={product}
+              setProduct={setProduct}
+            />
+            {product ? 
+              <FormCredit />
+            : 
+              ''
+              //<FormContribution /> 
+            }
+          </div>
+        </div>
+
+        <Form.Item>
+          <Button 
+            type="primary" 
+            htmlType="submit" 
+            loading={loadingBtn}
+          >
+            {loadingBtn ? '' : 'Отправить'}
+          </Button> 
+        </Form.Item>
+      </Form>
     </section>
   );
 }
