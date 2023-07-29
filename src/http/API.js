f28ca480d7b66f46aef5abc8465096c40d30d8b7
Ilya Korolev <82649037+Ilyakor657@users.noreply.github.com>
2023-07-29 18:15:44 +0400

    поменял url запроса

--- src/http/API.js
+++ src/http/API.js
@@ -1,6 +1,6 @@
 import $host from "./index";
 
 export const sendApplication = async (client, product) => {
-  const response = await $host.post("/index.php", {client, product})
+  const response = await $host.post("/createApplication", {client, product})
   return response
 }
